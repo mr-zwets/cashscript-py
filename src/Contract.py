f91e4aa8fabeeb4a47be765350af2d77ca1f8cbe
Mathieu Geukens <mr-zwets@protonmail.com>
2025-01-31 22:34:40 +0100

    add contract fixtures, add some initial tests

--- src/Contract.py
+++ src/Contract.py
@@ -3,11 +3,10 @@ class Contract:
         self.artifact = artifact
         self.constructor_args = constructor_args
         self.options = options
-        self.provider = "Placeholder provider"
-        self.address_type = "Placeholder addressType"
+        self.provider = "Placeholder self.provider"
+        self.address_type = options.get("addressType", "p2sh32") if options else "p2sh32"
         self.encoded_constructor_args = "Placeholder encodedConstructorArgs"
         self.redeem_script = "Placeholder redeemScript"
-        self.functions = "Placeholder functions"
         self.unlock = "Placeholder unlock"
         self.name = "Placeholder name"
         self.address = "Placeholder address"
@@ -16,14 +15,34 @@ class Contract:
         self.bytesize = "Placeholder bytesize"
         self.opcount = "Placeholder opcount"
 
+        expected_properties = ["abi", "bytecode", "constructorInputs", "contractName", "compiler"]
+        if not all(prop in artifact for prop in expected_properties):
+            raise ValueError("Invalid or incomplete artifact provided")
+
+        if len(artifact["constructorInputs"]) != len(constructor_args):
+            expected_types = [input["type"] for input in artifact["constructorInputs"]]
+            raise ValueError(
+                f"Incorrect number of arguments passed to {artifact['contractName']} constructor. "
+                f"Expected {len(artifact['constructorInputs'])} arguments ({expected_types}) "
+                f"but got {len(constructor_args)}"
+            )
+
+        # encodedConstructorArgs = encodeConstructorArguments(artifact, constructorArgs)
+
+        # self.redeem_script = generateRedeemScript(asmToScript(artifact.bytecode), encodedConstructorArgs)
+
+        self.name = artifact["contractName"]
+        # self.address = scriptToAddress(self.redeem_script, self.provider.network, self.address_type, false)
+        # self.token_address = scriptToAddress(self.redeem_script, self.provider.network, self.address_type, true)
+        # self.bytecode = binToHex(scriptToBytecode(self.redeem_script))
+        # self.bytesize = calculateBytesize(self.redeem_script)
+        # self.opcount = countOpcodes(self.redeem_script)
+
     async def get_balance(self):
         return "Placeholder bigint"
 
     async def get_utxos(self):
         return "Placeholder Utxo[]"
-
-    def create_function(self, abi_function, selector=None):
-        return "Placeholder ContractFunction"
 
     def create_unlocker(self, abi_function, selector=None):
         return "Placeholder ContractUnlocker"
--- test/test_Contract.py
+++ test/test_Contract.py
@@ -1,11 +1,83 @@
 import pytest
+import json
 import sys
 sys.path.append('../src')
 from src.Contract import Contract
 
+# Load artifacts
+def load_json_fixture(path):
+    with open(path, "r") as file:
+        return json.load(file)
+
+p2pkhArtifact = load_json_fixture("./test/fixture/p2pkh.json")
+twtArtifact = load_json_fixture("./test/fixture/transfer_with_timeout.json")
+hodlVaultArtifact = load_json_fixture("./test/fixture/hodl_vault.json")
+mecenasArtifact = load_json_fixture("./test/fixture/mecenas.json")
+boundedBytesArtifact = load_json_fixture("./test/fixture/bounded_bytes.json")
+
 @pytest.fixture
 def contract():
-    return Contract("Placeholder artifact", "Placeholder constructor_args", "Placeholder options")
+    constructor_args = ["", "", ""]
+    return Contract(twtArtifact, constructor_args, {"addressType": "p2sh32"})
+
+# Helper function for simulating placeholder values of a given size
+def placeholder(size):
+    return "x" * size 
+
+@pytest.mark.skip(reason="Functionality not yet implemented")
+def test_fail_incorrect_constructor_args():
+    with pytest.raises(ValueError):
+        Contract(p2pkhArtifact, [])
+
+    with pytest.raises(ValueError):
+        Contract(p2pkhArtifact, [20])
+
+    with pytest.raises(ValueError):
+        Contract(p2pkhArtifact, [placeholder(20), placeholder(20)])
+
+    with pytest.raises(ValueError):
+        Contract(p2pkhArtifact, [placeholder(19)])
+
+    with pytest.raises(ValueError):
+        Contract(p2pkhArtifact, [placeholder(21)])
+
+def test_fail_incomplete_artifact():
+    incomplete_artifacts = [
+        {**p2pkhArtifact, "abi": None},
+        {**p2pkhArtifact, "bytecode": None},
+        {**p2pkhArtifact, "constructorInputs": None},
+        {**p2pkhArtifact, "contractName": None},
+    ]
+    
+    with pytest.raises(ValueError):
+        Contract(incomplete_artifacts, [])
+
+def test_create_p2pkh_instance():
+    instance = Contract(p2pkhArtifact, [placeholder(20)])
+
+    assert isinstance(instance.address, str)
+    assert instance.name == p2pkhArtifact["contractName"]
+
+def test_create_transfer_with_timeout_instance():
+    constructor_args = [placeholder(65), placeholder(65), 1000000]
+    instance = Contract(twtArtifact, constructor_args)
+
+    assert isinstance(instance.address, str)
+    assert instance.name == twtArtifact["contractName"]
+
+def test_create_hodl_vault_instance():
+    constructor_args = [placeholder(65), placeholder(65), 1000000, 10000]
+    instance = Contract(hodlVaultArtifact, constructor_args)
+
+    assert isinstance(instance.address, str)
+    assert instance.name == hodlVaultArtifact["contractName"]
+
+def test_create_mecenas_instance():
+    constructor_args = [placeholder(20), placeholder(20), 1000000]
+    instance = Contract(mecenasArtifact, constructor_args)
+
+    assert isinstance(instance.address, str)
+    assert instance.name == mecenasArtifact["contractName"]
 
 @pytest.mark.asyncio
 async def test_get_balance(contract):
@@ -14,9 +86,6 @@ async def test_get_balance(contract):
 @pytest.mark.asyncio
 async def test_get_utxos(contract):
     assert await contract.get_utxos() == "Placeholder Utxo[]"
-
-def test_create_function(contract):
-    assert contract.create_function("Placeholder abi_function") == "Placeholder ContractFunction"
 
 def test_create_unlocker(contract):
     assert contract.create_unlocker("Placeholder abi_function") == "Placeholder ContractUnlocker"
